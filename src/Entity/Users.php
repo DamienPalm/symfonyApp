2c2f1e7216b2852da3ea5c7b8b7cdfac98e630d2
DamienPalm <palm.damien@gmail.com>
2023-06-11 08:39:03 +0200

    maj 11/06/2023

---
+++ src/Entity/Users.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UsersRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UsersRepository::class)]
+class Users
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 40)]
+    private ?string $Nom = null;
+
+    #[ORM\Column(length: 40)]
+    private ?string $Prenom = null;
+
+    #[ORM\Column(length: 40)]
+    private ?string $Email = null;
+
+    #[ORM\Column(length: 40)]
+    private ?string $Adresse = null;
+
+    #[ORM\Column(length: 40)]
+    private ?string $Tel = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->Nom;
+    }
+
+    public function setNom(string $Nom): static
+    {
+        $this->Nom = $Nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->Prenom;
+    }
+
+    public function setPrenom(string $Prenom): static
+    {
+        $this->Prenom = $Prenom;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->Email;
+    }
+
+    public function setEmail(string $Email): static
+    {
+        $this->Email = $Email;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->Adresse;
+    }
+
+    public function setAdresse(string $Adresse): static
+    {
+        $this->Adresse = $Adresse;
+
+        return $this;
+    }
+
+    public function getTel(): ?string
+    {
+        return $this->Tel;
+    }
+
+    public function setTel(string $Tel): static
+    {
+        $this->Tel = $Tel;
+
+        return $this;
+    }
+}
